8eb3b4a650d731612034e34e606a35f69ae5d001
lilinj2000 <lilinj2000@gmail.com>
2015-10-20 11:41:27 +0800

    update simnow account.

--- utest/env.sh
+++ utest/env.sh
@@ -1,8 +1,8 @@
 
-home_3rd=/app/3rd
+home_3rd=/llj/3rd
+home_boost=$home_3rd/boost
+home_ctp=$home_3rd/v6.3.6_20141230traderapi_linux64
 
-CTP_HOME=$home_3rd/v6.3.6_20141230traderapi_linux64
-
-export LD_LIBRARY_PATH=$home_3rd/boost/lib:$CTP_HOME
+export LD_LIBRARY_PATH=$home_boost/lib:$home_ctp
 
 
