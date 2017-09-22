2e73cd5d630b798326027f3c752494c25ad33199
lilinj2000 <lilinj2000@gmail.com>
2017-09-22 14:24:02 +0800

    message to json escape string test case done.

--- utest/MessageTest.cc
+++ utest/MessageTest.cc
@@ -3,82 +3,31 @@
 
 #include <memory>
 #include "gtest/gtest.h"
-#include "message/RspMessage.hh"
 #include "soil/Log.hh"
+#include "service/ThostFtdcUserApiStructPrint.hh"
 
 namespace cata {
 
-class MessageTest : public ::testing::Test {
- public:
-  MessageTest() {
+TEST(MessageTest, instrumentFieldTest) {
+  rapidjson::Document config;
+  soil::json::load_from_file(&config, "msg.json");
+  soil::log::init(config);
+
+  CThostFtdcInstrumentField rsp;
+  memset(&rsp, 0x0, sizeof(rsp));
+  SOIL_DEBUG_PRINT(rsp);
+
+  std::string rsp_str = fmt::format("{}", rsp);
+  std::string esp_str = soil::json::escape_string(rsp_str);
+  SOIL_DEBUG_PRINT(esp_str);
+
+  rapidjson::Document doc;
+  if (doc.Parse(esp_str).HasParseError()) {
+    SOIL_DEBUG_PRINT(soil::json::get_parse_error(doc, esp_str));
+    FAIL();
   }
 
-  void SetUp() {
-  }
-
-  void TearDown() {
-  }
-};
-
-TEST_F(MessageTest, rspMessageTest) {
-  CThostFtdcRspUserLoginField rsp_login;
-  CThostFtdcRspInfoField rsp_info;
-  memset(&rsp_login, 0x0, sizeof(rsp_login));
-  memset(&rsp_info, 0x0, sizeof(rsp_info));
-  int request_id = 100;
-  bool is_last = true;
-
-  std::unique_ptr<RspUserLoginMessage> rsp_login_message;
-  rsp_login_message.reset(new RspUserLoginMessage(&rsp_login,
-                                                  &rsp_info,
-                                                  request_id,
-                                                  is_last));
-
-  SOIL_INFO <<rsp_login_message->toString();
-
-  rsp_login_message.reset(new RspUserLoginMessage(nullptr,
-                                                  &rsp_info,
-                                                  request_id,
-                                                  is_last));
-
-  ASSERT_EQ(rsp_login_message->rspUserLogin(), nullptr);
-  SOIL_INFO <<rsp_login_message->toString();
-
-  rsp_login_message.reset(new RspUserLoginMessage(&rsp_login,
-                                                  nullptr,
-                                                  request_id,
-                                                  is_last));
-
-  ASSERT_EQ(rsp_login_message->rspInfo(), nullptr);
-  SOIL_INFO <<rsp_login_message->toString();
-
-  rsp_login_message.reset(new RspUserLoginMessage(nullptr,
-                                                  nullptr,
-                                                  request_id,
-                                                  is_last));
-
-  ASSERT_EQ(rsp_login_message->rspUserLogin(), nullptr);
-  ASSERT_EQ(rsp_login_message->rspInfo(), nullptr);
-  SOIL_INFO <<rsp_login_message->toString();
-
-  GTEST_SUCCEED();
-}
-
-TEST_F(MessageTest, rspQryInstrumentMessageTest) {
-  CThostFtdcInstrumentField rsp_instru;
-  CThostFtdcRspInfoField rsp_info;
-  memset(&rsp_instru, 0x0, sizeof(rsp_instru));
-  memset(&rsp_info, 0x0, sizeof(rsp_info));
-  int request_id = 100;
-  bool is_last = true;
-
-  std::cout <<rsp_instru;
-
-  std::stringstream ss;
-  ss <<rsp_instru;
-  json::Document d;
-  std::string str = ss.str();
-  json::fromString(ss.str(), &d);
+  SUCCEED();
 }
 
 };  // namespace cata
